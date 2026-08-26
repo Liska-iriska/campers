c7ea2bc05657e178e4b8240553d6f076bbbe28ce
Lis <genia.grishenko@gmail.com>
2026-08-26 15:44:26 +0300

    Add Header component

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,7 +1,38 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import css from "./Header.module.css";
 
 const Header = () => {
-  return <header className={css.header}></header>;
+  const pathname = usePathname();
+  return (
+    <header className={css.header}>
+      <div className={css.container}>
+        <Link href="/" aria-label="home">
+          <svg width={136} height={16} className={css.icon}>
+            <use href="/sprite.svg#icon-logo" />
+          </svg>
+        </Link>
+        <nav aria-label="Main Navigation">
+          <ul className={css.navigation}>
+            <li>
+              <Link href="/" className={pathname === "/" ? css.active : ""}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/catalog"
+                className={pathname === "/catalog" ? css.active : ""}
+              >
+                Catalog
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
