0a8bac85475078deab33622d7ff4cfdbf356bbfd
Lis <genia.grishenko@gmail.com>
2026-08-26 15:16:08 +0300

    Add Home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 //import type { Metadata } from "next";
 import TanStackProvider from "@/components/TanStackProvider/TanStackProvider";
 import Header from "@/components/Header/Header";
-import Footer from "@/components/Footer/Footer";
 import { Inter, Manrope } from "next/font/google";
 import "./globals.css";
 
@@ -52,7 +51,6 @@ export default function RootLayout({
         <TanStackProvider>
           <Header />
           <main>{children}</main>
-          <Footer />
         </TanStackProvider>
       </body>
     </html>
