b530bd4d0ebf473641749730f32ea3c203feafa4
Lis <genia.grishenko@gmail.com>
2026-08-29 11:43:41 +0300

    Edit sidebar form

--- app/catalog/@sidebar/page.tsx
+++ app/catalog/@sidebar/page.tsx
@@ -1,5 +1,11 @@
 import Sidebar from "@/components/Sidebar/Sidebar";
 
-export default function SidebarNotes() {
-  return <Sidebar />;
+interface Props {
+  searchParams: Promise<{ [key: string]: string | undefined }>;
+}
+
+export default async function SidebarNotes({ searchParams }: Props) {
+  const params = await searchParams;
+
+  return <Sidebar key={JSON.stringify(params)} />;
 }
